fac2c5bc4f2b48ada8ff8272fa58666ae7702b71
284281247 <284281247@qq.com>
2017-12-15 23:54:22 +0800

    modify the cal file

--- calculator.py
+++ calculator.py
@@ -4,25 +4,24 @@ import sys
 
 class Config(object):
 	def __init__(self, configfile):
-		self._config = {}
 		with open(configfile) as file:
-			for str in file.readline:
+			self.rate = 0.00
+			for str in file:
+				print(str)
 				str.strip()
 				name,num = str.split('=')
-				print(name,num)	
-				#if name == "JiShuL":
-				#	self.lowest = int(num)
-				#elif name == "JiShuH":
-				#	self.highest = int(num)
-				#else:
-				#	self.rate += int(num)
-
+				print(name,end='')
+				print(num)	
+				if name == "JiShuL":
+					self.lowest = float(num)
+				elif name == "JiShuH":
+					self.highest = float(num)
+				else:
+					self.rate += float(num)
+				
 
 if __name__ == '__main__':
-	try:
-		args = sys.argv[1:]
-		index_c = args.index('-c')
-		print("para %s is %s",index_c+1,args[index_c+1])
-		tex_config = Config(args[index_c+1])
-	except:
-		print("parameter error")
+	args = sys.argv[1:]
+	index_c = args.index('-c')
+	print("file is ",args[index_c+1])
+	tex_config = Config(args[index_c+1])
