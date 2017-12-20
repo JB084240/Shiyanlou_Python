4392d6dc96c26bdbd41e2788ee9b570698e133fc
284281247 <284281247@qq.com>
2017-12-20 15:50:06 +0800

    challenge3 teacher

--- Challenge3_CountSalary/challenge3.py
+++ Challenge3_CountSalary/challenge3.py
@@ -23,9 +23,9 @@ INCOME_TAX_QUICK_LOOKUP_TABLE = [
 class Args(object):
     
     def __init__(self):
-        self.args = sys.args[1:]
+        self.args = sys.argv[1:]
 
-    def _value-after_option(self, option):
+    def _value_after_option(self, option):
         try:
             index = self.args.index(option)
             return self.args[index +1]
@@ -101,7 +101,7 @@ class UserData(object):
 
     def _read_user_data(self):
         userdata_path = args.userdata_path
-        uerdata = []
+        userdata = []
         with open(userdata_path) as f:
             for line in f.readlines():
                 employee_id, income_string = line.strip().split(',')
@@ -116,3 +116,53 @@ class UserData(object):
     def __iter__(self):
         return iter(self.userdata)
 
+class IncomeTaxCalculator(object):
+    
+    def __init__(self, userdata):
+        self.userdata = userdata
+
+    @staticmethod
+    def calc_social_insurance_money(income):
+        if income < config.social_insurance_baseline_low:
+            return config.social_insurance_baseline_low * \
+                config.social_insurance_total_rate
+        
+        if income > config.social_insurance_baseline_high:
+            return config.social_insurance_baseline_high * \
+                config.social_insurance_total_rate
+
+        return income * config.social_insurance_total_rate
+
+    @classmethod
+    def calc_income_tax_and_remain(cls, income):
+        social_insurance_money = cls.calc_social_insurance_money(income)
+        real_income = income - social_insurance_money
+        taxable_part = real_income - INCOME_TAX_START_POINT
+        if taxable_part <= 0:
+            return '0.00', '{:.2f}'.format(real_income)
+        for item in INCOME_TAX_QUICK_LOOKUP_TABLE:
+            if taxable_part > item.start_point:
+                tax = taxable_part * item.tax_rate - item.quick_subtractor
+                return '{:.2f}'.format(tax), \
+                    '{:.2f}'.format(real_income -tax)
+
+
+    def calc_for_all_userdata(self):
+        result = []
+        for employee_id, income in self.userdata:
+            data = [employee_id, income]
+            social_insurance_money = '{:.2f}'.format(self.calc_social_insurance_money(income))
+            tax, remain = self.calc_income_tax_and_remain(income)
+            data += [social_insurance_money, tax, remain]
+            result.append(data)
+        return result
+
+    def export(self, default='csv'):
+        result = self.calc_for_all_userdata()
+        with open(args.export_path, 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerows(result)
+
+if __name__ == '__main__':
+    calculator = IncomeTaxCalculator(UserData())
+    calculator.export()
