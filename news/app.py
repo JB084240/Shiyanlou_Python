b8d13c8eb2508a673b0f25d23ba716fae5b0a6f3
284281247 <284281247@qq.com>
2017-12-25 12:20:00 +0800

    modify the app.py

--- news/app.py
+++ news/app.py
@@ -4,30 +4,91 @@
 from flask import Flask, render_template, abort
 import sys, os, json
 
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 app = Flask(__name__)
 app.config['TEMPLATE_AUTO_RELOAD'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/news'
+db = SQLAlchemy(app)
+
+##added for chanllenge database##
+
+class File(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    created_time = db.Column(db.DateTime)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    category = db.relationship('Category',backref=db.backref('file'))
+    content = db.Column(db.Text)
+    
+    def __init__(self, title, category, content):
+        self.title = title
+        self.created_time = datetime.utcnow()
+        self.category = category
+        self.content = content
+    
+    def __repr__(self):
+        return '<File %r>' % self.title
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<Category %r>' % self.name
+
+###end 
 
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'), 404
 
+
+@app.route('/files/<file_Id>')
+def file(file_Id):
+    file_dict = File.query.filter(id==file_Id)
+    if file_dict:
+        return render_template('file.html', file_dict=file_dict)
+    
+    abort(404)
+
+
 @app.route('/')
 def index():
-    fdict_list = []
+    f_index = File.query.all()
+    if f_index:
+        for f in f_index:
+            path = '/files/' + String(f.id)
+            f['path'] = path
+        return render_template('index.html', f_index=f_index)
+
+
+'''
+
+@app.route('/')
+def index():
     file_list = os.listdir('/home/shiyanlou/files/')
+    fdict_list = []
     for file in file_list:
         file_path = '/home/shiyanlou/files/' + file
-        print("file path is ", file_path)
         with open(file_path, 'r') as f:
             f_index = json.loads(f.read())
+            filename = file.split('.')
+            path = '/files/' + filename[0]
+            f_index['path'] = path
             fdict_list.append(f_index)
     return render_template('index.html', f_index=fdict_list)
+
 
 @app.route('/files/<filename>')
 def file_index(filename):
     filename_json = filename + '.json'
     file_path = '/home/shiyanlou/files/' + filename_json
-    print("filename is ", file_path)
     if os.path.isfile(file_path):
         if filename == 'helloshiyanlou' or filename == 'helloworld':
             with open(file_path, 'r') as file:
@@ -35,10 +96,11 @@ def file_index(filename):
    
                 cont_list = file_dict['content'].split('\\n')
                 file_dict['content'] = cont_list
-
+                
                 return render_template('file.html', file_dict=file_dict)
-
     abort(404)
+
+'''
 
 if __name__ == '__main__':
     app.run()
